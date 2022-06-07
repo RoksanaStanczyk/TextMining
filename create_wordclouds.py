ec700c6702f3671af5836a4fc929f71c01bc0abc
RoksanaStanczyk <rstanczyk24@gmail.com>
2022-06-07 10:22:50 +0200

    Add wordclouds for category

--- create_wordclouds.py
+++ create_wordclouds.py
@@ -14,3 +14,16 @@ def wordclouds(df: pd.DataFrame):
     plt.imshow(wc, interpolation='bilinear')
     plt.axis("off")
     plt.show()
+
+
+def wordclouds_category(df: pd.DataFrame, category: str):
+    df = df.loc[df['airline_sentiment'] == category]
+    text_all = " ".join(text for text in df.text.astype(str))
+    text_all = text_tokenizer(text_all)
+    text_all = " ".join(i for i in text_all)
+    wc = WordCloud()
+    wc.generate(text_all)
+    plt.figure(f"wordclouds for {category} category")
+    plt.imshow(wc, interpolation='bilinear')
+    plt.axis("off")
+    plt.show()
--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import pandas as pd
-from create_wordclouds import wordclouds
+from create_wordclouds import wordclouds, wordclouds_category
 from visualization import describe_data, token_visualization
 from classification import training_svm, training_RandomForestClassifier, training_LogisticRegression
 
 df = pd.read_csv('tweets_airline.csv')
 df = df[['text', 'airline_sentiment']]
-
+CATEGORY = 'neutral'
 # przedstawienie klas i ich liczby
 describe_data(df)
-#
 wordclouds(df)
+wordclouds_category(df, CATEGORY)
 token_visualization(df)
 training_svm(df)
 training_RandomForestClassifier(df)
 training_LogisticRegression(df)
-
